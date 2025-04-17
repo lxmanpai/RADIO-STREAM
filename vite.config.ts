dddd264e8d6f9c456e363ec86fa65c2bc46464f4
lxmanpai9 <165027360+lxmanpai9@users.noreply.github.com>
2025-04-17 12:30:52 +0530

    Github pages updated commands for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/RADIO-STREAM/",
+  base: "/RADIO-STREAM/", // 🔥 very important for github pages deployment
   plugins: [react()],
 });
